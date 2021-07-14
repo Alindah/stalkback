8ca7e107663802f9c2701d3f8073270b1edf16d6
Alinda <AlindaHeng@gmail.com>
2021-07-14 01:45:59 -0400

    Minor updates

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import validators, ValidationError
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField("Email ", [validators.Required("Enter your email address."), 
                                 validators.Email("Enter a proper email address.")])
     password = PasswordField("Password ", [validators.Required("Enter a password.")])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log in")
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     email = StringField("Email* ", [validators.Required("Enter your email address."), 
                                 validators.Email("Enter a proper email address.")])
     username = StringField("Username* ", [validators.Required("Enter a username.")])
@@ -18,7 +18,7 @@ class RegisterForm(Form):
     password_check = PasswordField("Re-enter password* ", [validators.Required("Retype your password.")])
     submit = SubmitField("Send")
 
-class SettingsForm(Form):
+class SettingsForm(FlaskForm):
     display_name = StringField("Display Name ")
     password = PasswordField("Old password ")
     password_new = PasswordField("New password ")
