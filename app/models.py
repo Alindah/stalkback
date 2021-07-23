9abc338df3dab55daaa776093170992cee7f5e89
Alinda <AlindaHeng@gmail.com>
2021-07-22 20:23:56 -0400

    Added new fields to UserModel

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager
 import hashlib
+from datetime import datetime
 
 # Create database and user model
 db_user = SQLAlchemy()
@@ -15,6 +16,14 @@ class UserModel(UserMixin, db_user.Model):
     display_name = db_user.Column(db_user.String(32))
     avatar_url = db_user.Column(db_user.String())
     password_hash = db_user.Column(db_user.String())
+    join_date = db_user.Column(db_user.DateTime, default=datetime.utcnow)
+    
+    # Optional info
+    birthday = db_user.Column(db_user.DateTime)
+    sex = db_user.Column(db_user.String(16), default="unset")
+
+    # Profile
+    tagline = db_user.Column(db_user.String(128), default="")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -35,6 +44,15 @@ login = LoginManager()
 @login.user_loader
 def load_user(id):
     return UserModel.query.get(int(id))
+
+class PostModel():
+    __tablename__ = 'posts'
+"""
+    id = db_user.Column(db_user.Integer, primary_key = True)
+    author = db_user.Column(db_user.String(32))
+    title = db_user.Column(db_user.String())
+    content = db_user.user.Column()
+    desc = db_user.Column(db_user.String())"""
 
 class CategoryModel():
     __tablename__ = 'categories'
