0d8b8b7fee34f3fdb27517b1c2f3f220cb19f770
Alinda <AlindaHeng@gmail.com>
2021-08-05 01:00:48 -0400

    Labeled forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, SelectField, HiddenField, TextAreaField, validators
 
+# ===== #
+# LOGIN #
+# ===== #
 class LoginForm(FlaskForm):
     email = StringField("Email", validators = [validators.Required("Enter your email address."), 
                                 validators.Email("Enter a proper email address.")],
@@ -9,6 +12,9 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
 
+# ============ #
+# REGISTRATION #
+# ============ #
 class RegisterForm(FlaskForm):
     email = StringField("Email*", [validators.Required("Enter your email address."), 
                                 validators.Email("Enter a proper email address.")],
@@ -19,6 +25,9 @@ class RegisterForm(FlaskForm):
     password_check = PasswordField("Re-enter password*", [validators.Required("Retype your password.")], description = "Re-type password*")
     submit = SubmitField("Submit")
 
+# ================ #
+# ACCOUNT SETTINGS #
+# ================ #
 class SettingsForm(FlaskForm):
     display_name = StringField("Display Name")
     password = PasswordField("Old password")
@@ -30,10 +39,16 @@ class DeleteAccount(FlaskForm):
     password_del = PasswordField(description = "Password")
     del_confirmation = SubmitField("Delete Account")
 
+# ========== #
+# SEARCH BAR #
+# ========== #
 class SearchBar(FlaskForm):
     search_query = StringField(description = "∞ Type here to start stalking...")
     search = SubmitField("Search ∞")
 
+# ============== #
+# NEW SUBMISSION #
+# ============== #
 class PostForm(FlaskForm):
     title = StringField("Title*", validators = [validators.Required("Title required")], description = "Enter a title")
     content = FileField("Content")
@@ -46,15 +61,23 @@ class PostForm(FlaskForm):
         super(PostForm, self).__init__()
         self.category.choices = user_cat
 
+# ====================== #
+# POST INTERACTION TOOLS #
+# ====================== #
 class PostInteraction(FlaskForm):
     post_id = HiddenField("Post ID")
     comment = TextAreaField("Comment", description = "Reply to post...")
 
+# ======= #
+# PROFILE #
+# ======= #
+# EDIT PROFILE
 class EditProfileForm(FlaskForm):
     tagline = TextAreaField("Tagline", description = "Enter a tagline. Affects all of your categories.")
     desc = TextAreaField("Description", description = "Enter a description. This only affects the current category.")
     submit = SubmitField("Save")
 
+# CATEGORY DROPDOWN
 class CategoryDropdown(FlaskForm):
     category = SelectField("Category")
 
@@ -62,5 +85,9 @@ class CategoryDropdown(FlaskForm):
         super(CategoryDropdown, self).__init__()
         self.category.choices = ['Select a category...'] + user_cat
 
+# ======= #
+# GENERIC #
+# ======= #
+# Form that trigger a POST request with no data attached
 class EmptyForm(FlaskForm):
     submit = SubmitField("Submit")
